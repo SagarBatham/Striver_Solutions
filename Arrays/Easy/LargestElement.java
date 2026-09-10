7dbb8d365848292a23600325ec6570c970e4cc81
Sagar Batham <shivsagar6912@gmail.com>
2026-09-10 22:50:12 +0530

    Add Largest and Second Largest Element programs; create .gitignore and settings.json

--- Arrays/Easy/LargestElement.java
+++ Arrays/Easy/LargestElement.java
@@ -5,13 +5,13 @@
 // Output: 6
 
 // Explanation: The largest element in array is 6
-public static Largest_Element{
+public class LargestElement{
     public static void main(String[] args){
         int[] nums={3, 3, 6, 1};
         System.out.println(Largest(nums));
     }
 
-    public int Largest(int[] arr){
+    public static int Largest(int[] arr){
         int max=Integer.MIN_VALUE;
         for(int i=0;i<arr.length;i++){
             if(max<arr[i]){
---
+++ Arrays/Easy/SecondLargestElement.java
@@ -0,0 +1,32 @@
+// Example 1
+
+// Input: nums = [8, 8, 7, 6, 5]
+
+// Output: 7
+
+// Explanation:
+
+// The largest value in nums is 8, the second largest is 7
+public class SecondLargestElement{
+    public static void main(String[] args){
+        int[] arr={8, 8, 7, 6, 5};
+        System.out.println(SecondLargest(arr));
+    }
+
+    public static int SecondLargest(int[] arr){
+        int max=Integer.MIN_VALUE;
+        int smax=max;
+        for(int e:arr){
+            if(max<e){
+                smax=max;
+                max=e;
+                System.out.println("if: "+max +" "+ smax);
+            }else if(smax<e && max!=e){
+                smax=e;
+                System.out.println("else if: "+max +" "+ smax);
+            }
+        }
+
+        return smax;
+    }
+}
